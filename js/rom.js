b1fe537bc77fc1b80f15807f2c49183ef9d1d39e
Arne Kvaleberg <arnekva@gmail.com>
2019-10-15 09:03:24 +0200

    Fix for MAT152

--- js/rom.js
+++ js/rom.js
@@ -6,7 +6,7 @@ let timeedit = gettimeedit()
 
 if(timeedit === null){
   $('#error').html("Du har ikke satt din egen timeedit ennå. Se <a href='hjelp.html'>her</a> for hjelp til oppsett. Du ser min som eksempel.");
-  console.log("whops")
+  console.log("No timetable has been set in the localStorage")
   timeedit = "https://cloud.timeedit.net/hvl/web/studbergen/ri10Yf506550Z9QY2XQ4757XZX076405702546Yy566Y5Q1g05Y10u5c47l5Q76X7Q70ZY7Y2ZQ0p9u0107xXZX4Q71l1502557Y651Qæl617W74WL3ZwX7ve87l5a5W78W6aX8WwE7Ynq5jX9n1W1Ww3p7wcxc6bmncUb9ceWjlXrn7rlrjQZ8e3tW6ra2wwK7XWc172aWWbn7rLW8vXpWmWet8nweXwcxbæ1wrjacWw3nnU6Emr1tacrXanjKw697WeWXW7vXXW55Won6cE65w26jWxX99196w1WX6a0W5v6KcXtnW7c)wv6v(V)aaw61xcXV5WX6X6KYoujxj(anj5w5v5Wop6b4a15p6w7175n9ZwWQQDeWQmdXr6tZ6Ewk00dZr0u1XtW43D06081E6C80F273E7912877DD9FE4.phtml"
 }else{
   console.log("Successfully got timeedit from localStorage")
@@ -23,7 +23,6 @@ var time = checkTime(date.getHours())
 //date.getMinutes() + ":" + date.getSeconds();
 let dato = formatDate(date)
 
-//lag for-løkke for å finne ikke null, break; når funnet
 let firstrun = true;
 for(let i = 1; i<12;i++){
 
@@ -44,6 +43,16 @@ for(let i = 1; i<12;i++){
   console.log(timeint)
   var node = document.querySelector('[title*=" ' + dato + ' '+ time + '"]');
   if(node !== null){
+    if(node.innerHTML.substr(0,3) === "MAT"){
+      //gjør custom greier for matte
+      //no clue if this works
+      $('#rom').html(node.innerHTML.substr(74,10));
+      $('#fag').html(node.innerHTML.substr(0,6));
+      $('#type').html(node.innerHTML.substr(64,10));
+      $('#tid').html("Klokken " + time);
+      console.log("Matte?")
+      break;
+    } else{
     console.log(node.innerHTML) // .substr(7)
     $('#rom').html(node.innerHTML.substr(29,6));
     $('#fag').html(node.innerHTML.substr(0,6));
@@ -52,6 +61,7 @@ for(let i = 1; i<12;i++){
     $('#tid').html("Klokken " + time);
 
     break;
+    }
   } else{
     console.log("Adding one hour")
     time = (date.getHours()+i) + ":" + "15"
